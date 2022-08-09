ce6638bcb2b81578f6d017c3bb62e7c322b44ba1
kylechui <kyle.chui+github@pm.me>
2022-08-08 18:35:13 -0700

    feat: Sample filetype config for Lua files.

---
+++ lua/nvim-surround/languages/lua.lua
@@ -0,0 +1,34 @@
+local config = require("nvim-surround.config")
+
+local nvim_surround_lua = vim.api.nvim_create_augroup("NvimSurroundLua", {})
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "lua",
+    callback = function()
+        require("nvim-surround").buffer_setup({
+            surrounds = {
+                ["f"] = {
+                    add = function()
+                        local result = config.get_input("Enter the function name: ")
+                        if result then
+                            return { { result .. "(" }, { ")" } }
+                        end
+                    end,
+                    find = function()
+                        return config.get_selection({ node = "function_call" })
+                    end,
+                    delete = "^([^=%s(]+%()().-(%))()$",
+                    change = {
+                        target = "^.-([%w_]+)()%b()()()$",
+                        replacement = function()
+                            local result = config.get_input("Enter the function name: ")
+                            if result then
+                                return { { result }, { "" } }
+                            end
+                        end,
+                    },
+                },
+            },
+        })
+    end,
+    group = nvim_surround_lua,
+})
