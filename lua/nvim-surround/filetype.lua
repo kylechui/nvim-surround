a5e2a5aa5ed09311fe1d3d92cd9ee8a8c3d4c3d6
kylechui <kyle.chui+github@pm.me>
2022-08-11 14:33:52 -0700

    feat: Add a whole host of new built-in filetypes.

--- lua/nvim-surround/filetype.lua
+++ lua/nvim-surround/filetype.lua
@@ -12,9 +12,89 @@ M.set_autocmd = function(filetype, callback)
     })
 end
 
+M.c = {
+    setup = function()
+        M.set_autocmd("c", function()
+            require("nvim-surround").buffer_setup({
+                surrounds = {
+                    ["f"] = {
+                        find = function()
+                            return config.get_selection({ node = "call_expression" })
+                        end,
+                    },
+                },
+            })
+        end)
+    end,
+}
+
+M.cs = {
+    setup = function()
+        M.set_autocmd("cs", function()
+            require("nvim-surround").buffer_setup({
+                surrounds = {
+                    ["f"] = {
+                        find = function()
+                            return config.get_selection({ node = "invocation_expression" })
+                        end,
+                    },
+                },
+            })
+        end)
+    end,
+}
+
 M.cpp = {
     setup = function()
         M.set_autocmd("cpp", function()
+            require("nvim-surround").buffer_setup({
+                surrounds = {
+                    ["f"] = {
+                        find = function()
+                            return config.get_selection({ node = "call_expression" })
+                        end,
+                    },
+                },
+            })
+        end)
+    end,
+}
+
+M.go = {
+    setup = function()
+        M.set_autocmd("go", function()
+            require("nvim-surround").buffer_setup({
+                surrounds = {
+                    ["f"] = {
+                        find = function()
+                            return config.get_selection({ node = "call_expression" })
+                        end,
+                    },
+                },
+            })
+        end)
+    end,
+}
+
+M.java = {
+    setup = function()
+        M.set_autocmd("java", function()
+            require("nvim-surround").buffer_setup({
+                surrounds = {
+                    ["f"] = {
+                        find = function()
+                            return config.get_selection({ node = "method_invocation" })
+                        end,
+                    },
+                },
+            })
+        end)
+    end,
+}
+
+M.javascript = {
+    setup = function()
+        M.set_autocmd("javascript", function()
             require("nvim-surround").buffer_setup({
                 surrounds = {
                     ["f"] = {
@@ -61,6 +141,54 @@ M.python = {
     end,
 }
 
+M.r = {
+    setup = function()
+        M.set_autocmd("r", function()
+            require("nvim-surround").buffer_setup({
+                surrounds = {
+                    ["f"] = {
+                        find = function()
+                            return config.get_selection({ node = "call" })
+                        end,
+                    },
+                },
+            })
+        end)
+    end,
+}
+
+M.ruby = {
+    setup = function()
+        M.set_autocmd("ruby", function()
+            require("nvim-surround").buffer_setup({
+                surrounds = {
+                    ["f"] = {
+                        find = function()
+                            return config.get_selection({ node = "call" })
+                        end,
+                    },
+                },
+            })
+        end)
+    end,
+}
+
+M.rust = { -- TODO: Try to add support for macros?
+    setup = function()
+        M.set_autocmd("rust", function()
+            require("nvim-surround").buffer_setup({
+                surrounds = {
+                    ["f"] = {
+                        find = function()
+                            return config.get_selection({ node = "call_expression" })
+                        end,
+                    },
+                },
+            })
+        end)
+    end,
+}
+
 M.typescriptreact = {
     setup = function()
         M.set_autocmd("typescriptreact", function()
@@ -74,6 +202,22 @@ M.typescriptreact = {
                     ["T"] = {
                         find = function()
                             return config.get_selection({ node = "jsx_element" })
+                        end,
+                    },
+                },
+            })
+        end)
+    end,
+}
+
+M.vim = {
+    setup = function()
+        M.set_autocmd("vim", function()
+            require("nvim-surround").buffer_setup({
+                surrounds = {
+                    ["f"] = {
+                        find = function()
+                            return config.get_selection({ node = "call_expression" })
                         end,
                     },
                 },
