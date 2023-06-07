23f4c4a4c60aee2a1b61cd3b42d88f471053885b
Ilia Ilinykh <ilyasyoy@gmail.com>
2023-06-07 08:36:03 +0300

    test: fix tests ane reformat

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
 all: test_lua 
 
 test_lua:
-		nvim --headless -u './tests/minimal_init.lua' -c "PlenaryBustedDirectory tests/ { minimal_init = './tests/minimal_init.lua' }"
-
+		nvim --headless -u './tests/minimal_init.lua' -c "PlenaryBustedDirectory ./tests/aliases_spec.lua { minimal_init = './tests/minimal_init.lua', sequential = true }"
+		nvim --headless -u './tests/minimal_init.lua' -c "PlenaryBustedDirectory ./tests/basics_spec.lua { minimal_init = './tests/minimal_init.lua', sequential = true }"
+		nvim --headless -u './tests/minimal_init.lua' -c "PlenaryBustedDirectory ./tests/configuration_spec.lua { minimal_init = './tests/minimal_init.lua', sequential = true }"
+		nvim --headless -u './tests/minimal_init.lua' -c "PlenaryBustedDirectory ./tests/dot_repeat_spec.lua { minimal_init = './tests/minimal_init.lua', sequential = true }"
+		nvim --headless -u './tests/minimal_init.lua' -c "PlenaryBustedDirectory ./tests/function_calls_spec.lua { minimal_init = './tests/minimal_init.lua', sequential = true }"
+		nvim --headless -u './tests/minimal_init.lua' -c "PlenaryBustedDirectory ./tests/html_tags_spec.lua { minimal_init = './tests/minimal_init.lua', sequential = true }"
+		nvim --headless -u './tests/minimal_init.lua' -c "PlenaryBustedDirectory ./tests/jumps_spec.lua { minimal_init = './tests/minimal_init.lua', sequential = true }"
 
--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,37 +1,37 @@
-vim.opt.expandtab = true
-
 -- this file create separate XDG path
 -- thus Lazy.nvim doesnt takes control over the loading
 local M = {}
 
 function M.root(root)
-  local f = debug.getinfo(1, "S").source:sub(2)
-  return vim.fn.fnamemodify(f, ":p:h:h") .. "/" .. (root or "")
+    local f = debug.getinfo(1, "S").source:sub(2)
+    return vim.fn.fnamemodify(f, ":p:h:h") .. "/" .. (root or "")
 end
 
 ---@param plugin string
 function M.load(plugin)
-  local name = plugin:match(".*/(.*)")
-  local package_root = M.root(".tests/site/pack/deps/start/")
-  if not vim.loop.fs_stat(package_root .. name) then
-    print("Installing " .. plugin)
-    vim.fn.mkdir(package_root, "p")
-    vim.fn.system({
-      "git",
-      "clone",
-      "--depth=1",
-      "https://github.com/" .. plugin .. ".git",
-      package_root .. "/" .. name,
-    })
-  end
+    local name = plugin:match(".*/(.*)")
+    local package_root = M.root(".tests/site/pack/deps/start/")
+    if not vim.loop.fs_stat(package_root .. name) then
+        print("Installing " .. plugin)
+        vim.fn.mkdir(package_root, "p")
+        vim.fn.system({
+            "git",
+            "clone",
+            "--depth=1",
+            "https://github.com/" .. plugin .. ".git",
+            package_root .. "/" .. name,
+        })
+    end
 end
 
 function M.setup()
-  vim.cmd([[set runtimepath=$VIMRUNTIME]])
-  vim.opt.runtimepath:append(M.root())
-  vim.opt.packpath = { M.root(".tests/site") }
+    vim.cmd([[set runtimepath=$VIMRUNTIME]])
+    vim.opt.runtimepath:append(M.root())
+    vim.opt.packpath = { M.root(".tests/site") }
 
-  M.load("nvim-lua/plenary.nvim")
+    M.load("nvim-lua/plenary.nvim")
+    vim.opt.expandtab = true
+    require("nvim-surround").setup()
 end
 
 M.setup()
