3ce9a02e3646b833fb5bd7eb59f4b55ce0d188d0
Kyle Chui <kyle.chui+github@pm.me>
2024-06-04 13:49:56 -0700

    fix: UTF-8 characters in surround table.

--- lua/nvim-surround/input.lua
+++ lua/nvim-surround/input.lua
@@ -1,7 +1,20 @@
 local M = {}
 
+-- Replaces terminal keycodes in an input character.
+---@param char string @The input character.
+---@return string @The formatted character.
+---@nodiscard
+M.replace_termcodes = function(char)
+    -- Do nothing to ASCII or UTF-8 characters
+    if #char == 1 or char:byte() >= 0x80 then
+        return char
+    end
+    -- Otherwise assume the string is a terminal keycode
+    return vim.api.nvim_replace_termcodes(char, true, true, true)
+end
+
 -- Gets a character input from the user.
----@return string|nil @The input character, or nil if a control character is pressed.
+---@return string|nil @The input character, or nil if an escape character is pressed.
 ---@nodiscard
 M.get_char = function()
     local ok, char = pcall(vim.fn.getcharstr)
@@ -9,7 +22,7 @@ M.get_char = function()
     if not ok or char == "\27" then
         return nil
     end
-    return char
+    return M.replace_termcodes(char)
 end
 
 -- Gets a string input from the user.
